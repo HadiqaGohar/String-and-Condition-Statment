2b98a9e2ce3fa3392ed212156881a8349c4ad032
HadiqaGohar <160311280+HadiqaGohar@users.noreply.github.com>
2024-03-23 13:35:11 -0700

    String & condition

---
+++ main.py
@@ -0,0 +1,21 @@
+#ESCAPE SEQUENCE CHARACTER:
+str1 = "Hadiqa\nGohar"
+str2 = "Hadiqa\tGohar"
+print (str1)
+print (str2)
+
+#CONCATINATION
+str1 = "hadiqa"
+str2 = "Gohar"
+finalStr = str1 + " " + str2
+print (finalStr)
+
+#LENGTH OF STR
+str1 = "hadiqa"
+len1 = len(str1)
+print(len1)       
+
+str2 = "Gohar"
+len2 = len(str2)
+print(len2)       
+
